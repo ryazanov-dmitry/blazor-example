48527113bf5fcc2dcaabb65f8ca8effa77353597
Dmitry Ryazanov <dimalat@gmail.com>
2022-03-08 17:46:33 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+
+app.Run();
